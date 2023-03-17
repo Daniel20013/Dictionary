e38f4426132c424721f02bef49c6d19d6a1781a1
Daniel20013 <126924076+Daniel20013@users.noreply.github.com>
2023-03-17 18:11:09 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function addToTheDictionary() {
+    let cntainer = document.querySelector("#wordsEntered");
+    let createWord = document.createElement("p");
+    let text = document.querySelector("#inputSave").value;
+    let pText = document.createTextNode(text);
+
+    createWord.appendChild(pText);
+    cntainer.appendChild(createWord);
+}
+
+function lookForTheWord() {
+    let dictionary = document.querySelectorAll("p");
+    let searchedWord = document.querySelector("#inputSearch").value;
+    let exists = false;
+  
+    for (let i = 0; i < dictionary.length; ++i) {
+        let text = dictionary[i].textContent;
+        if (text === searchedWord) {
+            exists = true;
+            break;
+        }
+    }
+
+    if (exists) {
+        alert("Cuvantul exista în dictionar!");
+    } else {
+        alert("Cuvantul nu exista in dictionar!");
+    }
+}
